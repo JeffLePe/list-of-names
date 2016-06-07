43c1fb8219f430e76dc93cb36284732912d62816
Edy Segura <ednardo@inatel.br>
2016-06-07 10:05:12 -0300

    Quick refactor in ListController.js

--- js/ListController.js
+++ js/ListController.js
@@ -11,13 +11,10 @@ var ListController = {
     },
     
     eventSubmitHandler: function (event) {
-        console.log('Event submit was trigged!');
-        var inputName = document.getElementById('name');
-        
-        ListController.addName(inputName.value);
-        
-        inputName.value = "";
         event.preventDefault(); //we need this to avoid the form submittion to a server
+        var inputName = document.getElementById('name');
+        ListController.addName(inputName.value);
+        inputName.value = "";
     },
     
     displayNames: function() {
